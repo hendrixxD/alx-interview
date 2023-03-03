3db35b2c8f2a1602f247d1c72f7486ab2cfa5c48
hendrixxD <maticsjnr@gmail.com>
2023-03-03 14:06:07 +0100

    nquenns

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,94 +1,51 @@
-#!/usr/bin/python3
-"""
-N-Queens Algorithms
-"""
+#!/usr/bin/env python3
 
-import sys
+""" N queens puzzle, challenge of placing N non attacking queens
+on a NxN chessboard
+This program solves the N queens problem """
+
+from sys import argv
 
 
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+def is_NQueen(cell: list) -> bool:
+    """ False if not N Queen, True if N Queen """
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
+            return False
+    return True
 
-    try:
-        n = int(args[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
+def solve_NQueens(dimension: int, row: int, cell: list, output: list):
+    """ Return result of N Queens recursively """
+    if row == dimension:
+        print(output)
+    else:
+        for column in range(0, dimension):
+            cell.append(column)
+            output.append([row, column])
+            if (is_NQueen(cell)):
+                solve_NQueens(dimension, row + 1, cell, output)
+            cell.pop()
+            output.pop()
 
-    row = 0
-    col = 0
-    solutions = []
-    placed_queens = []
-    stop = False
 
-    # iterating through rows and columns
-    while row < n:
-        back_track = False
-        while col < n:
-            # check if current column is safe
-            safe = True
-            for cord in placed_queens:
-                c = cord[1]
-                if (c == col or c + (row-cord[0]) == col or
-                        c - (row-cord[0]) == col):
-                    safe = False
-                    break
-
-            if not safe:
-                if col == n - 1:
-                    back_track = True
-                    break
-                col += 1
-                continue
-
-            # place queen
-            cords = [row, col]
-            placed_queens.append(cords)
-            # if it is the last row
-            # append solution and reset all to last unfinished row
-            # and last safe column in that row
-            if row == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        row = cord[0]
-                        col = cord[1]
-                for i in range(n - row):
-                    placed_queens.pop()
-                if row == n - 1 and col == n - 1:
-                    placed_queens = []
-                    stop = True
-                row -= 1
-                col += 1
-            else:
-                col = 0
-            break
-        if stop:
-            break
-        # on fail: go back to previous row
-        # and continue from last safe column + 1
-        if back_track:
-            row -= 1
-            while row >= 0:
-                col = placed_queens[row][1] + 1
-                del placed_queens[row]
-                if col < n:
-                    break
-                row -= 1
-            if row < 0:
-                break
-            continue
-        row += 1
-
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val)
-        else:
-            print(val)
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    output = []
+    cell = 0
+    solve_NQueens(int(N), cell, [], output)
